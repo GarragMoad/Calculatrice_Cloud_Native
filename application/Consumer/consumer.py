dff8e32920256a011907c5c1c57c5a1d1d95a5f3
Garrag Moad <garragmouad8@gmail.com>
2024-11-25 12:00:50 +0100

    kubernetes_services_Ok_CrosIssue

--- application/Consumer/consumer.py
+++ application/Consumer/consumer.py
@@ -1,11 +1,11 @@
 import pika, sys, os , redis
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq-service'))
     channel = connection.channel()
 
     # Connexion à Redis
-    redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+    redis_client = redis.StrictRedis(host='redis-service', port=6379, db=0)
 
     channel.queue_declare(queue='calcul')
 
